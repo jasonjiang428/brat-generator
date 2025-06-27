bbdb581af77a0f0b3137d0c317626e5f88b0ef3a
jasonjiang428 <jasonjiang428@gmail.com>
2025-06-27 22:37:17 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,3 @@
-import { Header } from "@/components/header"
-import { HeroSection } from "@/components/hero-section"
-import { GeneratorTool } from "@/components/generator-tool"
-import { HowItWorks } from "@/components/how-it-works"
-import { Gallery } from "@/components/gallery"
-import { BlogSection } from "@/components/blog-section"
-import { AboutSection } from "@/components/about-section"
-import { Footer } from "@/components/footer"
-import { AdBanner } from "@/components/ad-banner"
-
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <HeroSection />
-      <AdBanner position="top" />
-      <GeneratorTool />
-      <HowItWorks />
-      <Gallery />
-      <BlogSection />
-      <AboutSection />
-      <Footer />
-    </>
-  )
+  return <div>Hello, world!</div>
 }
