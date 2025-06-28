447e5d5af61601561360b3f3590518c591cdb905
jasonjiang428 <jasonjiang428@gmail.com>
2025-06-28 09:05:27 +0800

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  // 换成你的 vercel 域名或自定义域名
+  siteUrl: 'https://brat-generator-gold.vercel.app',
+  // 自动生成 robots.txt
+  generateRobotsTxt: true,
+  // 其他可选配置可以写在这里
+}
